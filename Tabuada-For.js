1021613d1fcd1e5759fed74acee921904628590f
digocarvalhoo <diogocarva25@gmail.com>
2024-07-04 18:26:28 -0300

    Tabuada até 10

---
+++ Tabuada-For.js
@@ -0,0 +1,9 @@
+
+
+for (let numero = 0; numero <= 10; numero++) {
+    console.log(`Tabuada do ${numero}:`);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numero} x ${i} = ${i * numero}`);
+    }
+    console.log('');
+}   
